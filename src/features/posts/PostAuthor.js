9118e174d087d25160434a8e80576810eeb43e7e
NifemiSoneye <nifemisoneye@gmail.com>
2026-01-02 08:05:13 -0800

    Advanced rtk usage chapter began

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -1,13 +1,22 @@
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersSlice";
-
+import { Link } from "react-router-dom";
 import React from "react";
 
 const PostAuthor = ({ userId }) => {
   const users = useSelector(selectAllUsers);
 
   const author = users.find((user) => user.id === userId);
-  return <span>by {author ? author.name : "Unknown Author"}</span>;
+  return (
+    <span>
+      by{" "}
+      {author ? (
+        <Link to={`/user/${userId}`}>{author.name}</Link>
+      ) : (
+        "Unknown Author"
+      )}
+    </span>
+  );
 };
 
 export default PostAuthor;
